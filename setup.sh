38889400ebc7b73c35894c4ca2e8ed787ce3a403
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-05-26 17:05:38 -0400

    updated setup to work with os x

--- setup.sh
+++ setup.sh
@@ -4,13 +4,18 @@
 export OPENCV_LIBDIR=/usr/local/lib
 export OPENCV_INCDIR=/usr/local/include
 
-if [ ! -f $OPENCV_LIBDIR/libopencv_core.so ]; then
-    echo "OPENCV LIBRARY NOT FOUND"
+suffix=so
+if [[ `uname` == 'Darwin' ]]; then
+    suffix=dylib
+fi
+
+if [ ! -f $OPENCV_LIBDIR/libopencv_core.${suffix} ]; then
+    echo "OPENCV LIBRARY NOT FOUND. Set env variable OPENCV_LIBDIR to where libopencv_core.${suffix} lives"
     return 1
 fi
 
 if [ ! -f $OPENCV_INCDIR/opencv/cv.h ]; then
-    echo "OPENCV INCLUDES NOT FOUND"
+    echo "OPENCV INCLUDES NOT FOUND. Please set env variable OPENCV_INCDIR to where opencv/cv.h lives"
     return 1
 fi
 
