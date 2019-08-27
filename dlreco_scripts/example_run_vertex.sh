441303a86c2bc9c81b5b697447b64057c129cef0
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-08-27 20:15:34 +0000

    bump larcv for roi tick flipping. add more working vertex configurations

--- dlreco_scripts/example_run_vertex.sh
+++ dlreco_scripts/example_run_vertex.sh
@@ -8,6 +8,22 @@ OUTDIR=vertexoutdir
 mkdir -p $OUTDIR
 
 # ==============================
+# RUN IN OLD CONFIGURATION
+# ==============================
+# inputs
+# -------
+# tagger_anaout_larcv.root: UBDL-made
+RECO=../LArCV/app/LArOpenCVHandle/cfg/mac/run_reco_server.py 
+#CFG=vertex_configs/prod_fullchain_ssnet_combined_newtag_extbnb_c10_union_server.cfg
+CFG=vertex_configs/prod_fullchain_ssnet_combined_newtag_extbnb_c10_union_server_alltrack.cfg
+SUPERA=supera-Run004999-SubRun000006.root
+SSNET=ssnetserveroutv2-larcv-Run004999-SubRun000006.root
+OLDTAGGER=taggeroutv2-larcv-Run004999-SubRun000006.root
+VERTOUT=vertexout_oldtagger_alltracks.root
+VERTANA=vertexana_oldtagger_alltracks.root
+#CMD="python $RECO $CFG $VERTANA $VERTOUT $SUPERA $SSNET $OLDTAGGER $OUTDIR"
+
+# ==============================
 # RUN ON MCC9-UBDL
 # ==============================
 # inputs
@@ -26,11 +42,17 @@ CFG=vertex_configs/prod_fullchain_mcc9ssnet_combined_newtag_extbnb_c10_union.cfg
 # ==============================
 RECO=../LArCV/app/LArOpenCVHandle/cfg/mac/run_reco_unified_ubdl_mix.py
 CFG=vertex_configs/prod_fullchain_unifiedubdlmix_combined_newtag_extbnb_c10_union.cfg
+#CFG=vertex_configs/prod_fullchain_unifiedubdlmix_combined_newtag_extbnb_c10_union_alltrack.cfg
 # set file names
-SUPERA=supera-Run004999-SubRun000006.root
 SSNET=ssnetserveroutv2-larcv-Run004999-SubRun000006.root
 DLTAGGER=dltagger-larcv-mcc9_v13_nueintrinsics_overlay_run1-Run004999-SubRun000006.root
+VERTOUT=vertexout_dltagger_ssnet.root
+VERTANA=vertexana_dltagger_ssnet.root
 CMD="python $RECO $CFG $VERTANA $VERTOUT $SSNET $DLTAGGER $OUTDIR"
+
+# ============
+# RUN COMMAND
+# ============
 echo "$CMD"
 $CMD
 
