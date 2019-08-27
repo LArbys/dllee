123b00f9f7afdb0cfbfface5205485ddc9ae02fc
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-08-27 13:17:55 -0400

    bump larcv. save vertex config and example that ran

--- dlreco_scripts/example_run_vertex.sh
+++ dlreco_scripts/example_run_vertex.sh
@@ -1,5 +1,11 @@
-
+#!/bin/bash
 # EXAMPLES
+
+# COMMON OUTPUTS
+VERTOUT=vertexout.root
+VERTANA=vertexana.root
+OUTDIR=vertexoutdir
+mkdir -p $OUTDIR
 
 # ==============================
 # RUN ON MCC9-UBDL
@@ -9,16 +15,11 @@
 # tagger_anaout_larcv.root: UBDL-made
 RECO=../LArCV/app/LArOpenCVHandle/cfg/mac/run_reco_mcc9.py 
 CFG=vertex_configs/prod_fullchain_mcc9ssnet_combined_newtag_extbnb_c10_union.cfg
-VERTOUT=vertexout.root
-VERTANA=vertexana.root
-OUTDIR=vertexoutdir
-
-mkdir -p $OUTDIR
 
 # w GDB (enter 'r' at prompt)
 #gdb --args python $RECO $CFG $VERTANA $VERTOUT out_larcv_test.root tagger_anaout_larcv.root $OUTDIR
 # w/o GDB
-python $RECO $CFG $VERTANA $VERTOUT out_larcv_test.root tagger_anaout_larcv.root $OUTDIR
+#python $RECO $CFG $VERTANA $VERTOUT out_larcv_test.root tagger_anaout_larcv.root $OUTDIR
 
 # ==============================
 # RUN ON MIX OF dllee_unified and UBDL inputs
@@ -26,8 +27,11 @@ python $RECO $CFG $VERTANA $VERTOUT out_larcv_test.root tagger_anaout_larcv.root
 RECO=../LArCV/app/LArOpenCVHandle/cfg/mac/run_reco_unified_ubdl_mix.py
 CFG=vertex_configs/prod_fullchain_unifiedubdlmix_combined_newtag_extbnb_c10_union.cfg
 # set file names
-SUPERA=supera_file.root
-SSNET=ssnet_server_file.root
-DLTAGGER=dltagger_larcv_out.root
-python $RECO $CFG $VERTANA $VERTOUT $SUPERA $SSNET $DLTAGGER $OUTDIR
+SUPERA=supera-Run004999-SubRun000006.root
+SSNET=ssnetserveroutv2-larcv-Run004999-SubRun000006.root
+DLTAGGER=dltagger-larcv-mcc9_v13_nueintrinsics_overlay_run1-Run004999-SubRun000006.root
+CMD="python $RECO $CFG $VERTANA $VERTOUT $SSNET $DLTAGGER $OUTDIR"
+echo "$CMD"
+$CMD
+
 
