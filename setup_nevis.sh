96099e5bf44a958fafe42f406a2251a33ccefcda
William Seligman <seligman@nevis.columbia.edu>
2019-10-14 14:06:04 -0400

    A few more changes to get things to compile on more systems. I tried to be Nevis-centric as little as possible, outside of the setup_nevis.sh script.

--- setup_nevis.sh
+++ setup_nevis.sh
@@ -10,3 +10,15 @@
 
 module load cmake root/06.08.06 opencv/3.1
 
+# Additional compilation notes:
+# - Even though "module load python/2.7" is part of the
+#   environment setup above, I still had to install
+#   numpy and python2-numpy for numpy to import properly
+#   in the build process. (This probably means that there's
+#   something in these scripts that assumes the location of
+#   the python site-packages directory.)
+# - Half-way through the build process, somehow 
+#   OPENCV_INCDIR and OPENCV_LIBDIR are reset. I had to do
+#     module unload opencv
+#     module load opencv
+#   for the build process to continue.
