58902b4ad627171df5f1db5d43abceb88ce94ff9
William Seligman <seligman@nevis.columbia.edu>
2019-10-14 14:00:21 -0400

    Made things a bit less system-dependent so code would compile (with some interruptions) at Nevis.

---
+++ setup_nevis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 10-Oct-2019 WGS
+# At Nevis we use environment modules to act to
+# set up our environment. 
+
+# Note that as of Oct-2019, these are the software versions
+# which will work in CentOS 7. Eventually there will be more
+# CentOS 7-compatible versions. 
+
+module load cmake root/06.08.06 opencv/3.1
+
