405735399471b0b65892049f326f2d4ab610f7f9
CVMFS UBOONE Account <cvmfsuboone@oasiscfs02.fnal.gov>
2019-10-22 23:04:07 -0500

    bump larcv add InterUtils to build scripts

--- build_for_ups.sh
+++ build_for_ups.sh
@@ -36,6 +36,9 @@ make -j4 || return 1
 cd $LARLITE_BASEDIR/UserDev/RecoTool
 make -j4 || return 1
 
+cd $LARLITE_BASEDIR/UserDev/InterUtils
+make -j4 || return 1
+
 cd $GEO2D_BASEDIR
 #git checkout develop
 make -j4 || return 1
