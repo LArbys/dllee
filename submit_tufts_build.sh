085451795c86ce9706ad1d5105068207b777d624
twongjirad <taritree.wongjirad@gmail.com>
2020-04-21 15:29:22 -0400

    bump larlitecv for larbysmc

--- submit_tufts_build.sh
+++ submit_tufts_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=dlunified_build
+#SBATCH --job-name=build_dlu_v1_0_5
 #SBATCH --output=dlunified_build.log
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4000
@@ -9,9 +9,10 @@
 
 module load singularity
 
-UNIFIED_DIR=/cluster/tufts/wongjiradlab/twongj01/production/dllee_unified_opencv3.1_copy2/
+UNIFIED_DIR=/cluster/tufts/wongjiradlab/twongj01/production/dllee_unified_v1_0_5/
 ROOT_DIR=/usr/local/root/root-6.16.00/
-CONTAINER=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_10022019.simg
+#CONTAINER=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_10022019.simg
+CONTAINER=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_dldependencies_pytorch1.3.sing
 
-singularity exec $CONTAINER bash -c "source $ROOT_DIR/bin/thisroot.sh && cd $UNIFIED_DIR && source setup.sh && source configure.sh && source clean.sh && source build.sh"
-#singularity exec $CONTAINER bash -c "source $ROOT_DIR/bin/thisroot.sh && cd $UNIFIED_DIR && source setup.sh && source configure.sh && source build.sh"
+#singularity exec $CONTAINER bash -c "source $ROOT_DIR/bin/thisroot.sh && cd $UNIFIED_DIR && source setup.sh && source configure.sh && source clean.sh && source build.sh"
+singularity exec $CONTAINER bash -c "source $ROOT_DIR/bin/thisroot.sh && cd $UNIFIED_DIR && source setup.sh && source configure.sh && source build.sh"
